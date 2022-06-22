5f9e46371663feeceded52243f6463185907d63c
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-22 21:30:33 +0900

    SpringBoot Setting
    project setting

---
+++ src/test/java/com/example/chocokcake/ChocokcakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chocokcake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocokcakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
