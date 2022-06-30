9a4f0c39c86092d60ee23f164b769de9842a4c03
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-30 23:30:59 +0900

    Cake Mapping
    user <-One To Many-> cake(cake_id)맵핑완료

--- src/main/java/com/example/chocokcake/entity/Cake.java
+++ src/main/java/com/example/chocokcake/entity/Cake.java
@@ -15,8 +15,14 @@ public class Cake {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "theme")
     private Long theme;
+
     @Column(name = "birth_day")
     private String brithDay;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
 }
