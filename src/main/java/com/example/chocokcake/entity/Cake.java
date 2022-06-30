ffbce26eee689bb37a3c7ee54b036f381a164197
soyeonkim0227 <soyeonkim0227@naver.com>
2022-06-30 23:36:25 +0900

    Cake Candle Mapping
    cake <-One To Many-> candle(cake-id) 맵핑완료

--- src/main/java/com/example/chocokcake/entity/Cake.java
+++ src/main/java/com/example/chocokcake/entity/Cake.java
@@ -3,6 +3,7 @@ package com.example.chocokcake.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Builder
 @Getter
@@ -15,8 +16,13 @@ public class Cake {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "theme")
     private Long theme;
+
     @Column(name = "birth_day")
     private String brithDay;
+
+    @OneToMany(mappedBy = "cake_id")
+    private List<Candle> cakeList;
 }
--- src/main/java/com/example/chocokcake/entity/Candle.java
+++ src/main/java/com/example/chocokcake/entity/Candle.java
@@ -7,18 +7,25 @@ import javax.persistence.*;
 @Builder
 @Getter
 @Setter
-@Table
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "candle")
 @Entity
 public class Candle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "theme")
     private Long theme;
+
     @Column(name = "postman")
     private String postman;
+
     @Column(name = "letter")
     private String letter;
+
+    @ManyToOne
+    @JoinColumn(name = "cake_id")
+    private Cake cakeId;
 }
